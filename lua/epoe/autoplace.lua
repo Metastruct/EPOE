8f32effe409924fe67554362baf894ac95b02864
Python1320 <Python1320@users.noreply.github.com>
2013-03-15 19:34:52 +0000

    This code is eye bleed

--- lua/epoe/autoplace.lua
+++ lua/epoe/autoplace.lua
@@ -1,13 +1,13 @@
 
-CreateClientConVar("epoe_autoplace", 0, true, false)
-CreateClientConVar("epoe_autoplace_margin", 2, true, false)
+local epoe_autoplace = CreateClientConVar("epoe_autoplace", 0, true, false)
+local epoe_autoplace_margin = CreateClientConVar("epoe_autoplace_margin", 2, true, false)
 
 timer.Create("epoe_autoplace", 1, 0, function()
-	if not epoe or not epoe.GUI then return end
-    local place = math.floor(GetConVarNumber("epoe_autoplace") or 0)
+	if not epoe or not IsValid(epoe.GUI) then return end
+    local place = math.floor(epoe_autoplace:GetFloat() or 0)
     if place < 1 or place > 9 then return end
 	epoe.GUI:SetSize(surface.ScreenWidth() / 2, surface.ScreenHeight() / 3)
-	local margin = math.floor(GetConVarNumber("epoe_autoplace_margin") or 0)
+	local margin = math.floor(epoe_autoplace_margin:GetFloat() or 0)
     local x, y = (place - 1) % 3 / 2, math.floor((place - 1) / 3) % 3 / 2
     local width, height = epoe.GUI:GetSize()
     local offset_x, offset_y = surface.ScreenWidth() - width - margin * 2, surface.ScreenHeight() - height - margin * 2
