4d60766f46ca1885c5ad587a49de8da6fc416446
Python1320 <Python1320@users.noreply.github.com>
2014-05-21 20:23:26 +0000

    Why has no one told me the filter wouldn't even work. Added concommands: epoe_filter_regex  - epoe_filter_regex <skip extra messages count, use 0 by default> <match string> epoe_filter_partial  - epoe_filter_partial <skip extra messages count, use 0 by default> <match string> epoe_filter_exact  - epoe_filter_exact <skip extra messages count, use 0 by default> <match string>
    epoe_filter_write_regex
     - epoe_filter_write_regex <skip extra messages count, use 0 by default> <match string>
    epoe_filter_write_partial
     - epoe_filter_write_partial <skip extra messages count, use 0 by default> <match string>
    epoe_filter_write_exact
     - epoe_filter_write_exact <skip extra messages count, use 0 by default> <match string>
     
    epoe_filter_reload
    epoe_filtering = 1 
    epoe_filtering_dbg = 1 ( def. 0 )

--- lua/epoe/client_filter.lua
+++ lua/epoe/client_filter.lua
@@ -3,11 +3,12 @@
 
 local e=epoe
 local Tag=e.Should_TagHuman -- or "EPOE"
-local push=table.insert
+
 ---------------
 -- Clientside Console UI
 ---------------
 local epoe_filtering=CreateClientConVar("epoe_filtering", "1", true, false)
+local epoe_filtering_dbg=CreateClientConVar("epoe_filtering_dbg", "0", true, false)
 
 e.filters=e.filters or {
 	hasany=false,
@@ -21,32 +22,64 @@ local find=e.filters.find
 local regex=e.filters.regex
 
 local function add(str,k)
+	
 	local strtype=str:sub(1,1)
+	local count=tonumber(str:sub(2,2))
+	local data = str:sub(3,-1)
+	
+	if not count then return false end
+	
 	if strtype=='!' then -- full
-		push(full,str:sub(2,-1))
+		full[data] = count
 	elseif strtype=='"' then -- string
-		str = str:match[[^"(.+)"$]] or str:match[[^"(.+)$]]
-		assert(str~=nil)
-		push(find,str)
+	
+		assert(data~="","empty string")
+		table.insert(find,{data,count})
+		
 	elseif strtype=='^' then -- regex
-		str=str:sub(2,-1)
+	
 		local ok,err=pcall(string.find,"test",str)
-		if not ok then ErrorNoHalt
-			"EPOE Regex parse failure: "
-		timer.Simple(0,e.internalPrint,"Filters: Line "..k..": Error in regex:"..tostring(err))
+		if not ok then 
+			ErrorNoHalt"EPOE Regex parse failure: " e.internalPrint("Filters: Line "..k..": Error in regex:"..tostring(err))
 			return false
 		end
-		push(regex,str)
-	elseif strtype=='#' or strtype=='-' or strtype=='/'   then -- regex
+		table.insert(regex,{data,count})
+		
+	elseif strtype=='#' or strtype=='-' or strtype=='/' then -- regex
 		-- comment
 	else
-		timer.Simple(0,e.internalPrint,"Filters: Line "..k..": Match type "..strtype.." is unknown!")
+		e.internalPrint("Filters: Line "..k..": Match type "..strtype.." is unknown!")
 		return false
 	end
 	e.filters.hasany = true
 	return true
 end
-concommand.Add("epoe_filter_addmanual",function(_,_,_,filter) if not add(filter,-1) then Msg"[EPOE] "print"Filter add failed" end end )
+local function ADD(X,Y)
+	concommand.Add("epoe_filter_"..X,function(_,cmd,_,filter) 
+		local x=filter:sub(2,2)
+		filter=filter:sub(1,1)..filter:sub(3,-1)
+		if x~=" " then
+			Msg"[EPOE] "print"Syntax: epoe_filter_* N PARAMSTR (N is skip extra messages count)"
+			return
+		end
+		
+		if not add(Y..filter,-1) then 
+			Msg"[EPOE] "print"Filter add failed"
+			return
+		end
+		e.internalPrint("Added filter with extraskips="..filter:sub(1,1).." and filterstr '"..Y..filter.."'")
+		if cmd:find"write" then
+			file.Append("epoe_filters.txt",Y..filter..'\n')
+		end
+	end , nil,"epoe_filter_"..X..' <skip extra messages count, use 0 by default> <match string>')
+end
+ADD("exact","!")
+ADD("partial",'"')
+ADD("regex","^")
+ADD("write_exact","!")
+ADD("write_partial",'"')
+ADD("write_regex","^")
+
 local function Reload()
 
 	table.Empty( full )
@@ -54,12 +87,12 @@ local function Reload()
 	table.Empty( regex )
 	e.filters.hasany = false
 	
-	local data=file.Read("epoe_filters.txt")
+	local data=file.Read("epoe_filters.txt",'DATA')
 	if not data then return end
 	local i=0
 	for k,str in pairs(string.Explode("\n",data)) do
 		str=string.TrimLeft(str) -- we may have spaces on right side
-		if str:len()>0 then
+		if str:len()>1 then
 			if add(str,k) then
 				i=i+1
 			end
@@ -73,19 +106,60 @@ Reload()
 
 concommand.Add("epoe_filter_reload",Reload)
 
+local skipnext=0
 local sfind=string.find
-hook.Add(Tag,Tag,function(txt,flags)
-	if not epoe_filtering:GetBool() or not e.filters.hasany then return end
-
-	if #full>0 and full[txt] 			then return false end
-
-	if #find>0 then
-		for _,str in ipairs(find) do
-			if sfind(txt,str,1,true) 	then return false end end
+local function ShouldFilter(txt,flags)
+	if skipnext>0 then
+		skipnext=skipnext-1
+		print("SKIP",txt)
+		return true
+	end
+	
+	--exact match
+	local count=full[txt]
+	if count then 
+		skipnext=count or 0
+		return true 
 	end
 
+	-- string match
+	if #find>0 then
+		for _,t in next,find do
+			local str=t[1]
+			if sfind(txt,str,1,true) then
+				skipnext=t[2]
+				return true 
+			end
+		end
+	end
+	
 	if #regex>0 then
-		for _,str in ipairs(regex) do
-			if sfind(txt,str) 			then return false end end
+		for _,t in ipairs(regex) do
+			local str=t[1]
+			if sfind(txt,str) then
+				skipnext=t[2]
+				return true 
+			end
+		end
+	end
+	
+end
+
+local skipshit
+hook.Add(Tag,Tag,function(txt,flags)
+	if not epoe_filtering:GetBool() 
+	or not e.filters.hasany 
+	or skipshit
+	then 
+		return 
+	end
+
+	if ShouldFilter(txt,flags) then 
+		if epoe_filtering_dbg:GetBool() then
+			skipshit=true
+			e.MsgC(Color(255,4,3),"~")
+			skipshit=false
+		end
+		return false 
 	end
 end)
